ed45d61a918cdaf103d20d5df2d616e073a95fca
Jessicanwanzeininuma <jessicanwanzeininuma@gmail.com>
2024-12-23 11:00:58 +0100

    swapping

---
+++ PYTHON/PYTHON 2/python 2.py
@@ -0,0 +1,9 @@
+name="jessica"
+age=float(input("enter age 1: "))
+age1= int(input("enter age 2: "))
+tage= age + age1
+print ("the first age is ",age)
+print ("the second age is", age1)
+print ("the tage is ", tage)
+print ("sliced name", name[1:-1])
+print ("characters in the string", len(name))
---
+++ PYTHON/PYTHON 2/swap.py
@@ -0,0 +1,7 @@
+age= input("enter the first value ")
+name= input("enter your first name ")
+invalid= age
+age= name
+name= invalid
+print ("the value of age after swaping", age)
+print ("the value of name after swaping", name)
