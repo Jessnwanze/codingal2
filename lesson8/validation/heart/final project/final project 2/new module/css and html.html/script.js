a682a2acdf944b4b059368093ef88bb2de68d7b5
Jessicanwanzeininuma <jessicanwanzeininuma@gmail.com>
2024-12-24 10:15:46 +0100

    commit

--- lesson8/validation/heart/final project/final project 2/new module/css and html.html/script.js
+++ lesson8/validation/heart/final project/final project 2/new module/css and html.html/script.js
@@ -1,23 +1,79 @@
+function getHistory() {
+    return document.getElementById("history-value").innerText
+}
+function printHistory(num) {
+    return document.getElementById("history-value").innerText = num
+}
+function getOutput() {
+    return document.getElementById("output-value").innerText
+}
+function prinOutput(num) {
+    if (num == "") {
+        document.getElementById("output-value").innerText = num
+    }
+    else {
+        document.getElementById("output-value").innerText = getFormattedNumber(num)
+    }
+}
+function getFormattedNumber(num) {
+    if (num == "-") {
+        return ""
+    }
+    var n = Number(num)
+    var value = n.toLocaleString("en")
+    return value
+}
+function reverseNumberFormat(num) {
+    return Number(num.replace(/,/g, ""))
+}
+var operator = document.getElementsByClassName("operator")
+for (var i = 0; i < operator.length; i++) {
+    operator[i].addEventListener("click", function () {
+        if (this.id == "clear") {
+            printHistory("")
+            prinOutput("")
+        }
+        else if (this.id == "backspace") {
+            var output = reverseNumberFormat(getOutput()).toString();
+            if (output) { //if output has a value
+                output = output.substr(0, output.length - 1);
+                prinOutput(output);
 
-const display = document.getElementById('display');
-const button = document.querySelectorAll('.button');
-
-let currentNumber = '';
-let previousNumber = '';
-let operation = '';
-
-button.forEach(button => {
-    button.addEventListener('click', () => {
-        const value = button.textContent;
-
-        if (lisNaN(value) || value === '.') {
-            if (value === '.' && currentNumber.includes('.')) return;
-            currentNumber += value;
-            display.value = currentNumber;
+            }
+        }
+        else {
+            var output = getOutput();
+            var history = getHistory();
+            if (output==""&&history =!"") {
+                if(isNaN(history[history.length-1])){
+                    history=history.substr(0,history.length-1);
+                }
+            }
+            if(output!="" || history!=""){
+                output= output==!? output: reverseNumberFormat(output);
+history= history + output;
+if (this.id=="="){
+    var result=eval(history);
+    prinOutput(result);
+    printHistory("");
+}
+else{
+    history= history + this.id;
+    printHistory(history);
+    prinOutput("");
+}
+            }
         }
 
-        
-    })
+    });
 }
-
-)
+var number = document.getElementsByClassName("number");
+for (var i =0; i<number.length;i++){
+    number[i].addEventListener('click',function(){
+        var output= reverseNumberFormat(getOutput());
+        if (output!=NaN){ // if output has a number
+output=output + this.id;
+prinOutput(output);
+        }
+    });
+}
